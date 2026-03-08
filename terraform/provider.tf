7ec80bffdf73c7156ee347812d36da0fd1cb1920
avadhoot <avadhootb@google.com>
2026-03-08 15:18:13 +0000

    Fix folder structure and track Terraform files

---
+++ terraform/provider.tf
@@ -0,0 +1,18 @@
+terraform {
+  backend "gcs" {
+    bucket  = "bucket-0077" # REPLACE THIS
+    prefix  = "terraform/state"
+  }
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "google" {
+  project = var.project_id
+  region  = var.region
+  zone    = var.zone
+}
